c95532965d6f657f2381b14403899b7c2ac08e6d
michrohub <u4724510@gmail.com>
2026-04-25 12:52:25 -0700

    add vercel.json file

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5000";
+// const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "https://api.gsps.online";
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false,
